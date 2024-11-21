8ba57b9c3e9e231e75618f901a9b7c16aba3ba4e
kailaskg <kailaskg1447@gmail.com>
2024-11-21 13:00:33 +0530

    navbar with sidenav layout created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'navbar',
+    loadComponent: () =>
+      import('./layout/layout.component').then((m) => m.LayoutComponent),
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Menus } from './model/menus';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule, MatNavList } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-layout',
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    RouterOutlet,
+    MatButtonModule,
+    CommonModule,
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {
+  menus: Menus[] = [
+    {
+      label: `Dashboard`,
+      redirectURL: '/home',
+      icon: 'dashboard',
+    },
+    {
+      label: 'Orders',
+      redirectURL: '/order',
+      icon: 'check_circle',
+    },
+    {
+      label: 'Products',
+      redirectURL: '/product',
+      icon: 'add_circle_outline',
+    },
+
+    {
+      label: 'Logout',
+      redirectURL: '/logout',
+      icon: 'exit_to_app',
+    },
+  ];
+
+  collapsed: boolean = false;
+
+  constructor(private router: Router) {}
+
+  collapsedState() {
+    this.collapsed = !this.collapsed;
+    console.log(this.collapsed);
+  }
+
+  sidenavWidth() {
+    return this.collapsed ? '65px' : '250px';
+  }
+
+  logout() {
+    sessionStorage.removeItem('token');
+    window.alert('Logged out successfully...');
+    this.router.navigate(['/']);
+  }
+}
